7f7a070f86ccae5cdeab38ba1017e0ea3bbb8dbb
anything896 <68326921+anything896@users.noreply.github.com>
2020-12-09 23:30:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import WelcomScreen from './screens/WelcomScreen.js'
+
+export default function App() {
+  return(
+    <WelcomScreen/>
+  )
+}
+
+
